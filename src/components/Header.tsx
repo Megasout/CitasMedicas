09e1555b52ff14ef59a0d5a61256bd5324a27b4c
Megasout <Megasoutt@gmail.com>
2023-08-17 23:57:25 -0300

    17/08

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="grid">
+            <h1 className="font-bold text-5xl text-center mx-auto md:w-3/4">Seguimiento Pacientes {''}
+                <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+        </header>
+    )
+}
