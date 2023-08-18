101603620db7087021735178e36ada8a22942d9c
Megasout <62775501+Megasout@users.noreply.github.com>
2023-08-18 12:41:56 -0300

    18/08 12:41

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,6 +1,18 @@
 function Formulario() {
     return (
-        <h2>Formulario</h2>
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+            <p className="text-lg mt-5">
+                Anade Pacientes y {' '}
+                <span className="text-red-700 font-bold">Administrarlos</span>
+            </p>
+            <form>
+                <div>
+                    <label>Nombre Mascota</label>
+                    <input type="text" placeholder="Nombre de la Mascota"></input>
+                </div>
+            </form>
+        </div>
     )
 }
 
